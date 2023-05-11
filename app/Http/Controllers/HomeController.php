c32b5f613ef8970dcd746a9f142b3c53eae0adcc
Jack Sari <janasarii@gmail.com>
2023-05-11 02:35:02 -0500

    add

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
 
-    public function hola () {
+    public function hola()
+    {
         return "API LARAVEL";
     }
 
@@ -26,5 +27,20 @@ class HomeController extends Controller
         }
 
         return $data;
+    }
+
+    public function home()
+    {
+        $statistics = DB::select("select s.id, s.`number`, s.name from statistics s;");
+        $why_be_part = DB::select("select wbp.id , wbp.title, wbp.name from why_be_part wbp;");
+        $reviews = DB::select("select r.id, r.name, r.url, r.country, r.comment from reviews r;");
+        $allies = DB::select("select a.id, a.name, a.url from allies a;");
+
+        return response()->json([
+            'statistics' => $statistics,
+            'why_be_part' => $why_be_part,
+            'reviews' => $reviews,
+            'allies' => $allies
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', [HomeController::class, 'hola']);
+Route::get('/', [HomeController::class, 'home']);
 Route::get('/courses', [HomeController::class, 'index']);
 
